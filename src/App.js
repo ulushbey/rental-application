577619d08ba64909c488f38e161718b7d1b9b6db
ulushbey <ulushbey.h@gmail.com>
2024-04-07 07:38:43 +0900

    BRR: Initial setup and creating pages

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import './App.css';
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+
+
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />}/>
+        <Route path="/register" element={<RegisterPage />}/>
+        <Route path="/login" element={<LoginPage />}/>
+      </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
